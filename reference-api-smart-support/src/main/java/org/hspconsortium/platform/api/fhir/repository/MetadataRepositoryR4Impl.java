0b5de7e931d0974790180e3d88609993f927eab0
Travis Cummings <travis@interopion.com>
2018-02-06 14:58:25 -0700

    Zipped up the datasource files
    Former-commit-id: 32312eb74cc6caaa185557a21a03b42208dc0a04 [formerly c84ca59518ea2677ada7fb52019897b2d51e6b5d]
    Former-commit-id: a1a442d233688fb576e37364e559a61e5e9f94cd

--- reference-api-smart-support/src/main/java/org/hspconsortium/platform/api/fhir/repository/MetadataRepositoryR4Impl.java
+++ reference-api-smart-support/src/main/java/org/hspconsortium/platform/api/fhir/repository/MetadataRepositoryR4Impl.java
@@ -1,55 +1,33 @@
 package org.hspconsortium.platform.api.fhir.repository;
 
-import org.hl7.fhir.r4.model.CodeableConcept;
-import org.hl7.fhir.r4.model.Coding;
-import org.hl7.fhir.r4.model.Extension;
-import org.hl7.fhir.r4.model.UriType;
-import org.hl7.fhir.r4.model.CapabilityStatement;
-import org.springframework.beans.factory.annotation.Value;
+import org.hl7.fhir.r4.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
 @Lazy
+@Component
 public class MetadataRepositoryR4Impl implements MetadataRepositoryR4 {
 
-    static private String SECURE_MODE = "secured";
-    static private String SECURE_MODE_MOCK = "mock";
-
-    @Value("${hspc.platform.api.security.mode}")
-    private String securityMode;
-
-    @Value("${hspc.platform.authorization.tokenUrl}")
-    private String tokenEndpointUri;
-
-    @Value("${hspc.platform.authorization.authorizeUrl}")
-    private String authorizationEndpointUri;
-
-    @Value("${hspc.platform.authorization.smart.registrationEndpointUrl}")
-    private String registrationEndpointUri;
-
-    @Value("${hspc.platform.authorization.smart.urisEndpointExtensionUrl}")
-    private String urisEndpointExtensionUrl;
-
-    @Value("${hspc.platform.authorization.smart.launchRegistrationUrl}")
-    private String launchRegistrationUrl;
+    @Autowired
+    private MetadataRepositoryConfig metadataRepositoryConfig;
 
     @Override
     @SuppressWarnings("Duplicates")
     public CapabilityStatement addCapabilityStatement(CapabilityStatement capabilityStatement) {
-        if (SECURE_MODE.equalsIgnoreCase(securityMode) || SECURE_MODE_MOCK.equalsIgnoreCase(securityMode)) {
+        if (metadataRepositoryConfig.isSecured()) {
             List<CapabilityStatement.CapabilityStatementRestComponent> restList = capabilityStatement.getRest();
 
             CapabilityStatement.CapabilityStatementRestComponent rest = restList.get(0);
             CapabilityStatement.CapabilityStatementRestSecurityComponent restSecurity = rest.getSecurity();
 
-            Extension conformanceExtension = new Extension(this.urisEndpointExtensionUrl);
-            conformanceExtension.addExtension(new Extension("authorize", new UriType(this.authorizationEndpointUri)));
-            conformanceExtension.addExtension(new Extension("token", new UriType(this.tokenEndpointUri)));
-            conformanceExtension.addExtension(new Extension("register", new UriType(this.registrationEndpointUri)));
-            conformanceExtension.addExtension(new Extension("launch-registration", new UriType(this.launchRegistrationUrl)));
+            Extension conformanceExtension = new Extension(metadataRepositoryConfig.getUrisEndpointExtensionUrl());
+            conformanceExtension.addExtension(new Extension("authorize", new UriType(metadataRepositoryConfig.getAuthorizeUrl())));
+            conformanceExtension.addExtension(new Extension("token", new UriType(metadataRepositoryConfig.getTokenUrl())));
+            conformanceExtension.addExtension(new Extension("register", new UriType(metadataRepositoryConfig.getRegistrationEndpointUrl())));
+            conformanceExtension.addExtension(new Extension("launch-registration", new UriType(metadataRepositoryConfig.getLaunchRegistrationUrl())));
 
             restSecurity.addExtension(conformanceExtension);
             CodeableConcept codeableConcept = new CodeableConcept();
