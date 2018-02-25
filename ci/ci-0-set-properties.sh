7aa08655da132adb808204c6840d08a6e2ce594c
Travis Cummings <travis@interopion.com>
2018-02-25 16:24:31 -0700

    New pipeline
    Former-commit-id: 4d17c94a9d58aa111faac57321a30bb859383d88 [formerly dd3e0e0701caf32272c28921c7b028b32edb9a6c] [formerly 4d17c94a9d58aa111faac57321a30bb859383d88 [formerly dd3e0e0701caf32272c28921c7b028b32edb9a6c] [formerly dc15264189b0a47533bbcbd1674a683d0d2b94e6 [formerly 8e324bba15b514922e28237d35bc16ea11ad196d]]]
    Former-commit-id: 07e2781b1103bc4804ae4431c194748c9790ad6a
    Former-commit-id: 7c00d7db1382d35eaa14108a0f934af3b1ae50fe [formerly 521f647dab18f7ec11ba4f26da2a64fbac0fcf90]
    Former-commit-id: e95c667930fe8d82a1a3c3eae4ca97b5c6e4c51e

--- ci/ci-0-set-properties.sh
+++ ci/ci-0-set-properties.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-CONFIG_FILE="../aws/task-config.json"
+CONFIG_FILE_DSTU2="../aws/task-config-dstu2.json"
+CONFIG_FILE_STU3="../aws/task-config-stu3.json"
 
 export PROJECT_FULL_NAME="${PROJECT_NAME}-${ACTIVE_ENV}"
 
 export PROJECT_VERSION=$(mvn -f ../pom.xml -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec)
 
-export PROJECT_PORT=$(cat ${CONFIG_FILE} | jq --raw-output '.config.port')
+export PROJECT_PORT_DSTU2=$(cat ${CONFIG_FILE_DSTU2} | jq --raw-output '.config.port')
+
+export PROJECT_PORT_DSTU2=$(cat ${CONFIG_FILE_STU3} | jq --raw-output '.config.port')
 
 export DOCKER_PROJECT_REPO="hspconsortium"
 
@@ -14,6 +17,8 @@ export DOCKER_IMAGE_COORDINATES=${DOCKER_PROJECT_REPO}/${PROJECT_NAME}:${PROJECT
 
 export AWS_REGION="us-west-2"
 
-export AWS_CONTAINER_MEMORY_RESERVE=$(cat ${CONFIG_FILE} | jq --raw-output '.config.memory')
+export AWS_CONTAINER_MEMORY_RESERVE_DSTU2=$(cat ${CONFIG_FILE_DSTU2} | jq --raw-output '.config.memory')
+echo "AWS_CONTAINER_MEMORY_RESERVE_DSTU2: $AWS_CONTAINER_MEMORY_RESERVE_DSTU2"
 
-export SPRING_PROFILES_ACTIVE="${ACTIVE_ENV}"
+export AWS_CONTAINER_MEMORY_RESERVE_STU3=$(cat ${CONFIG_FILE_STU3} | jq --raw-output '.config.memory')
+echo "AWS_CONTAINER_MEMORY_RESERVE_STU3: $AWS_CONTAINER_MEMORY_RESERVE_STU3"
--- ci/ci-local.sh
+++ ci/ci-local.sh
@@ -4,12 +4,26 @@ set -x
 
 # set values provided at build time
 export ACTIVE_ENV=test
-export PROJECT_NAME=bilirubin-cdshooks
+export PROJECT_NAME=api
 export TARGET_AWS_CLUSTER=hspc-test
 export ENC_PW_TEST=changeme
 
+# DSTU2
+export PROJECT_NAME=api-v5-dstu2
+export SPRING_PROFILES_ACTIVE="test,multitenant,dstu2"
 . ci-0-set-properties.sh
 . ci-1-prepare-sources.sh
-#. ci-2-docker-image.sh
+export PROJECT_PORT=${PROJECT_PORT_DSTU2}
+export AWS_CONTAINER_MEMORY_RESERVE=${AWS_CONTAINER_MEMORY_RESERVE_DSTU2}
+. ci-3-aws-task-update.sh
+. ci-4-aws-service-update.sh
+
+# STU3
+export PROJECT_NAME=api-v5-stu3
+export SPRING_PROFILES_ACTIVE="test,multitenant,stu3"
+. ci-0-set-properties.sh
+. ci-1-prepare-sources.sh
+export PROJECT_PORT=${PROJECT_PORT_STU3}
+export AWS_CONTAINER_MEMORY_RESERVE=${AWS_CONTAINER_MEMORY_RESERVE_STU3}
 . ci-3-aws-task-update.sh
 . ci-4-aws-service-update.sh
