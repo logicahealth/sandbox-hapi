d73f760084fcd61dee45352206e9ca7201a48a59
Travis Cummings <travis@interopion.com>
2018-02-25 16:24:31 -0700

    New pipeline
    Former-commit-id: 47936040f6ce287f2ca80d692aa5436f0d231d10

--- ci/ci-1-prepare-sources.sh
+++ ci/ci-1-prepare-sources.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-mvn -V -B -s settings.xml deploy -P DEPLOY-HSPC,hspc-nexus
-rm reference-api-webapp-multitenant/target/*-sources.jar
-rm reference-api-webapp-multitenant/target/*-javadoc.jar
+echo "nothing to do"
--- ci/ci-3-aws-task-update.sh
+++ ci/ci-3-aws-task-update.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+echo "PROJECT_FULL_NAME: $PROJECT_FULL_NAME"
+echo "DOCKER_IMAGE_COORDINATES: $DOCKER_IMAGE_COORDINATES"
+echo "PROJECT_PORT: $PROJECT_PORT"
+echo "AWS_CONTAINER_MEMORY_RESERVE: $AWS_CONTAINER_MEMORY_RESERVE"
+
 ../aws/build-template.sh $PROJECT_FULL_NAME $DOCKER_IMAGE_COORDINATES $PROJECT_PORT $AWS_CONTAINER_MEMORY_RESERVE
 
 echo $(aws ecs register-task-definition --region ${AWS_REGION} --family ${PROJECT_FULL_NAME} --cli-input-json file://../aws/task-definition.json)
