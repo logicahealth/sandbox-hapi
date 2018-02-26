3a8063f42e8d14b77b9c9022c7d309e70c5306b7
Travis Cummings <travis@interopion.com>
2018-02-25 22:23:40 -0700

    new build process
    Former-commit-id: 8a29be779ff5d3e6f6cf67730cabde4e481d6567

--- ci/ci-local.sh
+++ ci/ci-local.sh
@@ -3,27 +3,24 @@
 set -x
 
 # set values provided at build time
-export ACTIVE_ENV=test
-export PROJECT_NAME=api
-export TARGET_AWS_CLUSTER=hspc-test
-export ENC_PW_TEST=changeme
+export TARGET_ENV=test
 
-# DSTU2
-export PROJECT_NAME=api-v5-dstu2
-export SPRING_PROFILES_ACTIVE="test,multitenant,dstu2"
+export FHIR_VERSION=dstu2
 . ci-0-set-properties.sh
 . ci-1-prepare-sources.sh
-export PROJECT_PORT=${PROJECT_PORT_DSTU2}
-export AWS_CONTAINER_MEMORY_RESERVE=${AWS_CONTAINER_MEMORY_RESERVE_DSTU2}
-. ci-3-aws-task-update.sh
-. ci-4-aws-service-update.sh
 
-# STU3
-export PROJECT_NAME=api-v5-stu3
-export SPRING_PROFILES_ACTIVE="test,multitenant,stu3"
+export FHIR_VERSION=stu3
 . ci-0-set-properties.sh
 . ci-1-prepare-sources.sh
-export PROJECT_PORT=${PROJECT_PORT_STU3}
-export AWS_CONTAINER_MEMORY_RESERVE=${AWS_CONTAINER_MEMORY_RESERVE_STU3}
-. ci-3-aws-task-update.sh
-. ci-4-aws-service-update.sh
+
+. ci-2-build-project.sh
+
+export FHIR_VERSION=dstu2
+. ci-3-docker-image.sh
+. ci-4-aws-task-update.sh
+. ci-5-aws-service-update.sh
+
+export FHIR_VERSION=stu3
+. ci-3-docker-image.sh
+. ci-4-aws-task-update.sh
+. ci-5-aws-service-update.sh
