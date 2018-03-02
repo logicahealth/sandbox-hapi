f29b504400e741635ecbf8d70bcd43438e9d02fa
Travis Cummings <travis@interopion.com>
2018-03-02 16:12:04 -0700

    Added subscription support
    Former-commit-id: ce99ebe3666a6cf0a0455763323910d2ca11e86c

--- reference-api-mysql/src/main/java/org/hspconsortium/platform/api/fhir/HapiJpaConfigR4.java
+++ reference-api-mysql/src/main/java/org/hspconsortium/platform/api/fhir/HapiJpaConfigR4.java
@@ -3,6 +3,7 @@ package org.hspconsortium.platform.api.fhir;
 
 import ca.uhn.fhir.jpa.config.BaseJavaConfigR4;
 import ca.uhn.fhir.jpa.search.ISearchCoordinatorSvc;
+import ca.uhn.fhir.jpa.subscription.resthook.SubscriptionRestHookInterceptor;
 import ca.uhn.fhir.jpa.util.SubscriptionsRequireManualActivationInterceptorR4;
 import ca.uhn.fhir.rest.server.interceptor.IServerInterceptor;
 import org.hspconsortium.platform.api.fhir.multitenant.search.MultiTenantSearchCoordinatorSvcImpl;
@@ -17,6 +18,11 @@ public class HapiJpaConfigR4 extends BaseJavaConfigR4 {
     @Bean(autowire = Autowire.BY_TYPE)
     public IServerInterceptor subscriptionSecurityInterceptor() {
         return new SubscriptionsRequireManualActivationInterceptorR4();
+    }
+
+    @Bean
+    public SubscriptionRestHookInterceptor subscriptionRestHookInterceptor() {
+        return new SubscriptionRestHookInterceptor();
     }
 
     @Bean(autowire = Autowire.BY_TYPE)
