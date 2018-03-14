532be48554b92240dbe1f5f5b8e1915d0d41da10
Travis Cummings <travis@interopion.com>
2018-03-14 16:55:00 -0600

    Removed Mercury operations and jars
    Former-commit-id: c97b629c4320c32ad82893b61dc1d0e41cfbb3ba

--- reference-api-webapp-multitenant/src/main/java/org/hspconsortium/platform/api/interceptor/PubSubInterceptor.java
+++ reference-api-webapp-multitenant/src/main/java/org/hspconsortium/platform/api/interceptor/PubSubInterceptor.java
@@ -1,50 +1,62 @@
 package org.hspconsortium.platform.api.interceptor;
 
-import ca.uhn.fhir.rest.api.RestOperationTypeEnum;
-import ca.uhn.fhir.rest.server.interceptor.IServerInterceptor;
-import com.ihc.mercury.sm.wsclient.MercuryClient;
-import com.ihc.mercury.sm.wsclient.MercuryClientFactory;
+import ca.uhn.fhir.rest.api.server.RequestDetails;
+import com.google.common.base.Strings;
 import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hspconsortium.platform.api.controller.HapiFhirServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import java.util.Properties;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Set;
 
 @Component
-public class PubSubInterceptor extends SubscriptionSupportBase implements IServerInterceptor {
-    @Value("${hspc.platform.messaging.pubsub.subscriptionEndpointEx}")
-    private String subscriptionEndpointEx;
+@Profile("multitenant")
+public class PubSubInterceptor extends SubscriptionSupportBase {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PubSubInterceptor.class);
 
-    @Value("${hspc.platform.messaging.pubsub.user}")
-    private String user;
+    @Value("${hspc.platform.messaging.pubsub.enabled:false}")
+    private boolean enabled;
 
-    @Value("${hspc.platform.messaging.pubsub.password}")
-    private String password;
+    @Value("${hspc.platform.messaging.pubsub.subscription.channel.endpoint:}")
+    private String forwardUrl = "";
 
-    @Value("${hspc.platform.messaging.pubsub.securityMode}")
-    private String securityMode;
+    @Value("#{'${hspc.platform.messaging.pubsub.forSandboxes:}'.split(',')}")
+    private Set<String> forSandboxes;
 
-    @Value("${hspc.platform.messaging.pubsub.enabled}")
-    private String enabled;
-
+    @Value("#{'${hspc.platform.messaging.pubsub.subscription.forResources:}'.split(',')}")
+    private Set<String> forResources;
 
     @Override
-    public void incomingRequestPreHandled(RestOperationTypeEnum theOperation, ActionRequestDetails theProcessedRequest) {
-        if (Boolean.valueOf(enabled)) {
-            if (getMercuryClient() == null) {
-                Properties mercuryProperties = new Properties();
-                mercuryProperties.setProperty(MercuryClient.CLIENT_ID, user);
-                mercuryProperties.setProperty(MercuryClient.CLIENT_SECRET, password);
-                mercuryProperties.setProperty(MercuryClient.SERVER_URL, subscriptionEndpointEx);
-                mercuryProperties.setProperty(MercuryClient.API_SECURITY_MODE, securityMode);
-                System.setProperty(MercuryClient.API_SECURITY_MODE, securityMode);
-                setMercuryClient(MercuryClientFactory.getClientFactory(mercuryProperties).getMercuryClient());
-            }
-            IBaseResource iBaseResource = theProcessedRequest.getResource();
-            if (iBaseResource != null) {
-                super.sendToMercury(theOperation, iBaseResource);
+    public boolean outgoingResponse(RequestDetails theRequestDetails, IBaseResource theResponseObject, HttpServletRequest theServletRequest, HttpServletResponse theServletResponse) {
+        if (enabled) {
+            switch (theRequestDetails.getRestOperationType()) {
+                case CREATE:
+                case UPDATE:
+                case DELETE:
+                    if (!Strings.isNullOrEmpty(forwardUrl)) {
+                        if (forResources.contains(theResponseObject.getClass().getSimpleName())) {
+                            // todo match the desired sandbox
+                            String requestSandbox = HapiFhirServlet.getTenantPart(theServletRequest.getServletPath());
+                            if (forSandboxes.contains(requestSandbox)) {
+                                LOGGER.info("Matched resource: " + theResponseObject.getIdElement().getIdPart());
+                                handleResource(theResponseObject, forwardUrl);
+                            }
+                        }
+                    }
+                    break;
             }
         }
-        super.incomingRequestPreHandled(theOperation, theProcessedRequest);
+
+        return super.outgoingResponse(theRequestDetails, theResponseObject, theServletRequest, theServletResponse);
+    }
+
+    @Override
+    protected void handleResource(IBaseResource resource, String resourceEndpoint) {
+        sendViaHTTP(resource, resourceEndpoint);
     }
 }
