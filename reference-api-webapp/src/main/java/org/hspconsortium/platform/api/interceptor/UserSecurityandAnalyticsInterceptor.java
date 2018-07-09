f27e9f198824455c80bfbffc5022b9acd6347a31
Jacob Crump <>
2018-07-09 10:04:32 -0600

    Making changes to call made to sandbox manager
    Former-commit-id: 0788593c69e90c29a084fee2eb61aa9158036aff [formerly 6417565725892d8912bbe82884d056fd0f50f79c] [formerly 0788593c69e90c29a084fee2eb61aa9158036aff [formerly 6417565725892d8912bbe82884d056fd0f50f79c] [formerly d7e2509864f1fb40d48e73c6faff162951d6bfac [formerly 6c0f5aad7ef36128db71bd522246e2332d66ccd2]]]
    Former-commit-id: 41c54db181f88928463f7c5a7f47320626153708
    Former-commit-id: f55ac6b7e8e0eed350c723fb340ef7922220092c [formerly 78c5a2b26b1c72b1e62b76cccce32145e262d4ec]
    Former-commit-id: ccaad87ed34912bb1fa5dce5dd8d613abeb6901a

--- reference-api-webapp/src/main/java/org/hspconsortium/platform/api/interceptor/UserSecurityandAnalyticsInterceptor.java
+++ reference-api-webapp/src/main/java/org/hspconsortium/platform/api/interceptor/UserSecurityandAnalyticsInterceptor.java
@@ -10,9 +10,13 @@ import javax.servlet.http.HttpServletResponse;
 import ca.uhn.fhir.rest.server.exceptions.BaseServerResponseException;
 import ca.uhn.fhir.rest.server.interceptor.InterceptorAdapter;
 import ca.uhn.fhir.rest.server.exceptions.AuthenticationException;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpException;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 import org.hspconsortium.platform.api.security.TenantInfoRequestMatcher;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,38 +40,60 @@ public class UserSecurityandAnalyticsInterceptor extends InterceptorAdapter {
 
     @Override
     public boolean outgoingResponse(RequestDetails theRequestDetails, ResponseDetails theResponseDetails, HttpServletRequest theServletRequest, HttpServletResponse theServletResponse) throws AuthenticationException {
-        theResponseDetails.getResponseCode();
-        handleCallToSandboxAPI(theServletRequest);
+
+        Integer responseCode = theResponseDetails.getResponseCode();
+        handleCallToSandboxAPI(theServletRequest, theServletRequest, responseCode);
         return true;
     }
 
     @Override
-    public BaseServerResponseException preProcessOutgoingException(RequestDetails theRequestDetails, Throwable theException, HttpServletRequest theServletRequest) throws ServletException {
-        theException.getMessage();
-        handleCallToSandboxAPI(theServletRequest);
-        return null;
+    public boolean handleException(RequestDetails theRequestDetails, BaseServerResponseException theException, HttpServletRequest theServletRequest, HttpServletResponse theServletResponse) {
+        Integer responseCode = theException.getStatusCode();
+        // TODO: find better solution to the if statement below
+        // This if statement is so a second call isn't made to the sandbox manager api because an exception was thrown earlier
+        if (!theException.getCause().getMessage().equals("User does not have privileges to this sandbox.")) {
+            handleCallToSandboxAPI(theServletRequest, theServletRequest, responseCode);
+        }
+        return true;
     }
 
-    private void handleCallToSandboxAPI(HttpServletRequest request) {
-        checkIfUserIsAuthorized(request);
+    private void handleCallToSandboxAPI(HttpServletRequest request, HttpServletRequest servletRequest, Integer responseCode) {
+        checkIfUserIsAuthorized(request, servletRequest, responseCode);
 
     }
 
-    private void checkIfUserIsAuthorized(HttpServletRequest request) {
-        String[] parameters = request.getRequestURI().split("/");
-        String tenant = parameters[1];
-        String secured = parameters[2];
-        String endpointCall = parameters[3];
+    private void checkIfUserIsAuthorized(HttpServletRequest request, HttpServletRequest servletRequest, Integer responseCode) {
+        String urlPath = request.getRequestURL().toString();
+        String[] parameters = urlPath.split("/");
+        String domain = parameters[2];
+        String tenant = parameters[3];
+        String secured = parameters[4];
+        String resource = parameters[5];
+
         if (secured.equals("open")) {
             checkIfOpenEndpointIsAllowed(tenant);
-        } else if (endpointCall.equals("metadata")) {
-            // do nothing, this will always be open
-        } else {
+            secured = "false";
+        }  else {
+            secured = "true";
+        }
+        if (!resource.equals("metadata")) {
             try {
                 String authHeader = request.getHeader("Authorization");
                 HttpClient httpclient = HttpClients.createDefault();
-                HttpPost httppost = new HttpPost(sandboxManagerApiUrl + transactionPath + tenant);
+                HttpPost httppost = new HttpPost(sandboxManagerApiUrl + transactionPath);
+                String body = "{" +
+                        "\"url\": \"" + urlPath + "\"," +
+                        "\"domain\": \"" + domain + "\"," +
+                        "\"resource\": \"" + resource + "\"," +
+                        "\"method\": \"" + request.getMethod() + "\"," +
+                        "\"response_code\": \"" + responseCode + "\"," +
+                        "\"ip_address\": \"" + getRemoteAddress(servletRequest) + "\"," +
+                        "\"tenant\": \"" + tenant + "\"," +
+                        "\"secured\": \"" + secured + "\"" +
+                        "}";
+                httppost.setEntity(new StringEntity(body, ContentType.APPLICATION_JSON));
                 httppost.setHeader("Authorization", authHeader);
+                httppost.setHeader("Content-type", "application/json");
                 HttpResponse returned = httpclient.execute(httppost);
                 if (returned.getStatusLine().getStatusCode() == 401) {
                     throw new Exception();
@@ -84,5 +110,14 @@ public class UserSecurityandAnalyticsInterceptor extends InterceptorAdapter {
             throw new RuntimeException("Sandbox " + tenant + " does not have an open endpoint.");
         }
     }
+
+    private String getRemoteAddress(HttpServletRequest httpServletRequest) {
+        String remoteAddress = httpServletRequest.getHeader("X-FORWARDED-FOR");
+        if (StringUtils.isEmpty(remoteAddress)) {
+            remoteAddress = httpServletRequest.getRemoteAddr();
+        }
+        return remoteAddress;
+    }
+
 
 }
