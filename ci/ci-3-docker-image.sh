ce5dd9d8eacfe9fdc67adfdb1fff6a0750aec16d
Travis Cummings <travis@interopion.com>
2018-07-31 14:50:11 -0600

    added docker-compose.yml
    Former-commit-id: c59404db87f4861cc933833226827025d2a91f09 [formerly c34f46586224e45a8dfdb73605da42f37691bb58]
    Former-commit-id: a630da08f1d6990bef5cda6b45f64745d3d0b353

--- ci/ci-3-docker-image.sh
+++ ci/ci-3-docker-image.sh
@@ -2,5 +2,6 @@
 
 docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
 cd ../docker; ./build.sh $DOCKER_IMAGE_COORDINATES $TARGET_ENV
+cd ../ci
 echo "docker push..."
 docker push "$DOCKER_IMAGE_COORDINATES"
