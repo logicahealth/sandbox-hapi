7301c3dbf65c31491dd7e81b72b5c7eac5012e64
Jacob Crump <>
2018-08-07 11:55:16 -0600

    Finished $submit-data endpoint for MeasureReport Resource provider
    Former-commit-id: a9f2140538e94d316c065efdcc4c00d09a8dd0fb

--- reference-api-mysql/src/main/java/org/hspconsortium/platform/api/fhir/config/HapiJpaConfigStu3.java
+++ reference-api-mysql/src/main/java/org/hspconsortium/platform/api/fhir/config/HapiJpaConfigStu3.java
@@ -5,6 +5,7 @@ import ca.uhn.fhir.jpa.search.ISearchCoordinatorSvc;
 import ca.uhn.fhir.jpa.util.SubscriptionsRequireManualActivationInterceptorDstu3;
 import ca.uhn.fhir.rest.server.interceptor.IServerInterceptor;
 import org.hspconsortium.platform.api.fhir.multitenant.search.MultiTenantSearchCoordinatorSvcImpl;
+import org.hspconsortium.platform.api.fhir.rp.MeasureReportResourceProviderHspc;
 import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -43,8 +44,8 @@ public class HapiJpaConfigStu3 extends BaseJavaConfigDstu3 {
     @Override
     @Lazy
     public ca.uhn.fhir.jpa.rp.dstu3.MeasureReportResourceProvider rpMeasureReportDstu3() {
-        ca.uhn.fhir.jpa.rp.dstu3.MeasureReportResourceProvider retVal;
-        retVal = new ca.uhn.fhir.jpa.rp.dstu3.MeasureReportResourceProvider();
+        MeasureReportResourceProviderHspc retVal;
+        retVal = new MeasureReportResourceProviderHspc();
         retVal.setContext(fhirContextDstu3());
         retVal.setDao(daoMeasureReportDstu3());
         return retVal;
