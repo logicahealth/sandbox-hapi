8649f88dc6256a400a2fb43c15052378fba18320
Jacob Crump <>
2018-08-07 11:55:16 -0600

    Finished $submit-data endpoint for MeasureReport Resource provider
    Former-commit-id: cfb1b3e23d4423010293aefb227099de913f11e2 [formerly 1e5f92b03a82f563d43baa16c12b0574142d3b33]
    Former-commit-id: 8100df8eb0bfb304e4ae1220f757cc6cb3f9faa2

---
+++ reference-api-mysql/src/main/java/org/hspconsortium/platform/api/fhir/rp/MeasureReportResourceProviderHspc.java
@@ -0,0 +1,104 @@
+package org.hspconsortium.platform.api.fhir.rp;
+
+import ca.uhn.fhir.jpa.rp.dstu3.MeasureReportResourceProvider;
+import ca.uhn.fhir.rest.annotation.IdParam;
+import ca.uhn.fhir.rest.annotation.Operation;
+import ca.uhn.fhir.rest.annotation.OperationParam;
+import ca.uhn.fhir.rest.api.server.RequestDetails;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.client.interceptor.BearerTokenAuthInterceptor;
+import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.dstu3.model.IdType;
+import org.hl7.fhir.dstu3.model.MeasureReport;
+import org.hl7.fhir.dstu3.model.Resource;
+import org.hl7.fhir.instance.model.api.IAnyResource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MeasureReportResourceProviderHspc extends MeasureReportResourceProvider {
+
+    @Operation(name = "$submit-data", idempotent = true)
+    public Resource submitData(
+            RequestDetails details,
+            @IdParam IdType theId,
+            @OperationParam(name="measure-report", min = 1, max = 1, type = MeasureReport.class) MeasureReport report,
+            @OperationParam(name="resource") List<IAnyResource> resources)
+    {
+        Bundle transactionBundle = new Bundle().setType(Bundle.BundleType.TRANSACTION);
+
+        /*
+            TODO - resource validation using $data-requirements operation
+            (params are the provided id and the measurement period from the MeasureReport)
+            TODO - profile validation ... not sure how that would work ...
+            (get StructureDefinition from URL or must it be stored in Ruler?)
+        */
+
+        for (IAnyResource resource : resources) {
+            Resource res = (Resource) resource;
+            if (res instanceof Bundle) {
+                for (Bundle.BundleEntryComponent entry : createTransactionBundle((Bundle) res).getEntry()) {
+                    transactionBundle.addEntry(entry);
+                }
+            }
+            else {
+                // Build transaction bundle
+                transactionBundle.addEntry(createTransactionEntry(res));
+            }
+        }
+
+        IGenericClient client = this.getContext().newRestfulGenericClient(details.getFhirServerBase());
+
+        if (details.getHeader("Authorization") != null) {
+            String[] splitAuth = details.getHeader("Authorization").split(" ");
+            String token = splitAuth[1];
+            BearerTokenAuthInterceptor authInterceptor = new BearerTokenAuthInterceptor(token);
+            client.registerInterceptor(authInterceptor);
+        }
+
+        return client.transaction().withBundle(transactionBundle).execute();
+    }
+
+    private Bundle createTransactionBundle(Bundle bundle) {
+        Bundle transactionBundle;
+        if (bundle != null) {
+            if (bundle.hasType() && bundle.getType() == Bundle.BundleType.TRANSACTION) {
+                transactionBundle = bundle;
+            }
+            else {
+                transactionBundle = new Bundle().setType(Bundle.BundleType.TRANSACTION);
+                if (bundle.hasEntry()) {
+                    for (Bundle.BundleEntryComponent entry : bundle.getEntry()) {
+                        if (entry.hasResource()) {
+                            transactionBundle.addEntry(createTransactionEntry(entry.getResource()));
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            transactionBundle = new Bundle().setType(Bundle.BundleType.TRANSACTION).setEntry(new ArrayList<>());
+        }
+
+        return transactionBundle;
+    }
+
+    private Bundle.BundleEntryComponent createTransactionEntry(Resource resource) {
+        Bundle.BundleEntryComponent transactionEntry = new Bundle.BundleEntryComponent().setResource(resource);
+        if (resource.hasId()) {
+            transactionEntry.setRequest(
+                    new Bundle.BundleEntryRequestComponent()
+                            .setMethod(Bundle.HTTPVerb.PUT)
+                            .setUrl(resource.getId())
+            );
+        }
+        else {
+            transactionEntry.setRequest(
+                    new Bundle.BundleEntryRequestComponent()
+                            .setMethod(Bundle.HTTPVerb.POST)
+                            .setUrl(resource.fhirType())
+            );
+        }
+        return transactionEntry;
+    }
+}
