c15bfd0bddf301d36a04dcaee43f0eac4d339900
Jacob Crump <>
2018-09-13 17:41:52 -0600

    Adding unit testing to each component. A lot of tests left to implement.
    Former-commit-id: 4613d23a1480029e66d816c9b82a44039ce76088 [formerly 18ea89a7fef846639b329917674be331129a20d0] [formerly 4613d23a1480029e66d816c9b82a44039ce76088 [formerly 18ea89a7fef846639b329917674be331129a20d0] [formerly 5a299a1056c77668372b0a8c64015e354a066ee5 [formerly 3c87aa1a81fa412ff92c6f9b640f162d7bbd1fdc]]]
    Former-commit-id: a304f4c521d17347ca7215d8b55be717a119329b
    Former-commit-id: 3a6332acd784af57c6cae5e1004b9e45b1398c18 [formerly 6bff6e643815edcf311dc70c3036309aa281c1b2]
    Former-commit-id: 7709ce1f9e6a9f61cbf71bd4113792da104e38cc

---
+++ reference-api-webapp/src/test/java/org/hspconsortium/platform/api/controller/HapiFhirServletTest.java
@@ -0,0 +1,17 @@
+package org.hspconsortium.platform.api.controller;
+
+import org.junit.Test;
+
+import javax.servlet.ServletException;
+
+public class HapiFhirServletTest {
+
+    private HapiFhirServlet hapiFhirServlet = new HapiFhirServlet();
+
+
+    @Test
+    public void initialize() throws ServletException {
+
+    }
+
+}
