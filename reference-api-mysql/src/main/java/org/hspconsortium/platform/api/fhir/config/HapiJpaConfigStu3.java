4cb7ae2c2b7a403f6bdfb903c1b556edb830e97b
Jacob Crump <>
2018-09-21 14:51:59 -0600

    Adding CQF ruler functionality.
    Former-commit-id: 7e4ae7fdc896105b3ba4fe3701393ff8fa360e45

--- reference-api-mysql/src/main/java/org/hspconsortium/platform/api/fhir/config/HapiJpaConfigStu3.java
+++ reference-api-mysql/src/main/java/org/hspconsortium/platform/api/fhir/config/HapiJpaConfigStu3.java
@@ -3,14 +3,21 @@ package org.hspconsortium.platform.api.fhir.config;
 import ca.uhn.fhir.jpa.config.BaseJavaConfigDstu3;
 import ca.uhn.fhir.jpa.search.ISearchCoordinatorSvc;
 import ca.uhn.fhir.jpa.util.SubscriptionsRequireManualActivationInterceptorDstu3;
+import ca.uhn.fhir.rest.server.IResourceProvider;
 import ca.uhn.fhir.rest.server.interceptor.IServerInterceptor;
+import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hspconsortium.platform.api.fhir.multitenant.search.MultiTenantSearchCoordinatorSvcImpl;
-import org.hspconsortium.platform.api.fhir.rp.MeasureReportResourceProviderHspc;
+import org.opencds.cqf.providers.FHIRMeasureResourceProvider;
+import org.opencds.cqf.providers.JpaDataProvider;
 import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+import java.util.List;
 
 //import org.hspconsortium.platform.api.fhir.util.HspcFhirInstanceValidator;
 
@@ -40,14 +47,23 @@ public class HapiJpaConfigStu3 extends BaseJavaConfigDstu3 {
 //        return val;
 //    }
 
-    @Bean(name="myMeasureReportRpDstu3")
-    @Override
-    @Lazy
-    public ca.uhn.fhir.jpa.rp.dstu3.MeasureReportResourceProvider rpMeasureReportDstu3() {
-        MeasureReportResourceProviderHspc retVal;
-        retVal = new MeasureReportResourceProviderHspc();
-        retVal.setContext(fhirContextDstu3());
-        retVal.setDao(daoMeasureReportDstu3());
-        return retVal;
-    }
+//    @Bean(name="myMeasureRpDstu3")
+//    @Override
+//    @Lazy
+//    public ca.uhn.fhir.jpa.rp.dstu3.MeasureResourceProvider rpMeasureDstu3() {
+////        MeasureResourceProviderHspc retVal;
+////        retVal = new MeasureResourceProviderHspc();
+////        retVal.setContext(fhirContextDstu3());
+////        retVal.setDao(daoMeasureDstu3());
+////        return retVal;
+//        WebApplicationContext myAppCtx = HapiFhirServletContextHolder.getInstance().getMyAppCtx();
+//        myAppCtx.refresh();
+//        List<IResourceProvider> beans = myAppCtx.getBean("myResourceProvidersDstu3", List.class);
+//        JpaDataProvider provider = new JpaDataProvider(beans);
+//        FHIRMeasureResourceProvider retVal;
+//        retVal = new FHIRMeasureResourceProvider(provider);
+//        retVal.setContext(fhirContextDstu3());
+//        retVal.setDao(daoMeasureDstu3());
+//        return retVal;
+//    }
 }
