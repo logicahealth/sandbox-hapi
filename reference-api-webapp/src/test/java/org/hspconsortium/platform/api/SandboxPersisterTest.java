bb4829d516feef5cdf7487d97016082ee037ea9d
Jacob Crump <>
2018-10-03 12:31:43 -0600

    Changing to HAPI version 3.4.0
    Former-commit-id: a2bb5704a23a6315cabf5f90ca7307e4e3bb4002 [formerly 9f8a210c01e14483cea1bb5b2f7dd77da42609cd] [formerly a2bb5704a23a6315cabf5f90ca7307e4e3bb4002 [formerly 9f8a210c01e14483cea1bb5b2f7dd77da42609cd] [formerly 4e8b3345225a1fc8b5c15d99b993b231a2eb6dee [formerly c3535d11b91542691ef35c48789846f7f2b6e964]]]
    Former-commit-id: 3848333f3bd050a030d80f8dd008c90f81e353d3
    Former-commit-id: c84ea647b34db893109a9d064dbff4e32b2a6a7d [formerly f1158a5d2224dedd2edd764b5dfee8e8cfa6e282]
    Former-commit-id: cd83b1472fc43dcc80303537b5bd4e7e30b36dc4

---
+++ reference-api-webapp/src/test/java/org/hspconsortium/platform/api/SandboxPersisterTest.java
@@ -0,0 +1,25 @@
+package org.hspconsortium.platform.api;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class SandboxPersisterTest {
+
+//    @Test
+//    public void cloneSandboxTest() throws Exception {
+//        String dump = "mysqldump";
+//        String[] cmdarray = {"/bin/sh","-c", dump};
+//        Process pr = Runtime.getRuntime().exec(cmdarray);
+//        Integer outcome = pr.waitFor();
+//
+//        BufferedReader in2 = new BufferedReader(new InputStreamReader(pr.getErrorStream()));
+//        String error = IOUtils.toString(in2);
+//        BufferedReader in = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+//        System.out.println(error);
+//        System.out.println(IOUtils.toString(in));
+//
+//    }
+}
