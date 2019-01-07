c06bec3919b9e828bb4e36eb4e3bf612da20765b
Jacob Crump <>
2019-01-07 13:53:29 -0700

    Scripts for reindexing 3.4.0 databases.
    Former-commit-id: e4e7da47d3e66e3625ed8748c5970c19cca5cee2 [formerly 2ad6f2097c371b1f55335797d39e83da94a02582]
    Former-commit-id: dd5137a37d852960968a1bdb34310dfbad641196

--- 360-migration/run-fhir-server.sh
+++ 360-migration/run-fhir-server.sh
@@ -7,17 +7,14 @@ environment=$2
 sandbox_name=$3
 jasypt_password=$4
 
-echo "using $1 profile..."
-
-jar="../reference-api-webapp/target/hspc-reference-api-webapp-*.jar"
+jar="hspc-reference-api-webapp-*.jar"
 
 set -x
 
-echo "running..."
-#-Djasypt.encryptor.password=${jasypt_password} \
 java \
   -Dspring.profiles.active=${profile},multitenant,${environment} \
   -Xms256M \
   -Xmx512M \
   -Dhspc.platform.api.sandbox.name=${sandbox_name} \
+  -Djasypt.encryptor.password=${jasypt_password} \
   -jar ${jar} &> output.log &
