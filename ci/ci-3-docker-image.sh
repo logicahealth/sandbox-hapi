cf7eab2434dd9fc86ca2ca45bd91060d02e7ee6b
Jacob Crump <jacob@interopion.com>
2019-02-14 11:13:56 -0700

    Further upgrades to springboot 2.0 and adding header licenses to all files.
    Former-commit-id: 93acbd0579b10017fbe7cfaa9bb85ea2aab7f288 [formerly ab73b27e9d2ee88dbe905c6e96714d9aa9adce32] [formerly 93acbd0579b10017fbe7cfaa9bb85ea2aab7f288 [formerly ab73b27e9d2ee88dbe905c6e96714d9aa9adce32] [formerly 12eba0b41241beca5e5a3f743f6dbe9481310642 [formerly c25f2ca76b280302fda5d3a7e8dce85422fde0bc]]]
    Former-commit-id: c00f4f38cb64b1bc52c35b05eaf5305366bebf04
    Former-commit-id: b3cc8118a336630a4ae62e8b31e6e88a67ed287a [formerly d01945610306169ba0a011f797f9531de9c0f781]
    Former-commit-id: ce310f07eecc1bdfd89a2d2b51db9f57315dcf8d

--- ci/ci-3-docker-image.sh
+++ ci/ci-3-docker-image.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env bash
+#
+#  * #%L
+#  *
+#  * %%
+#  * Copyright (C) 2014-2019 Healthcare Services Platform Consortium
+#  * %%
+#  * Licensed under the Apache License, Version 2.0 (the "License");
+#  * you may not use this file except in compliance with the License.
+#  * You may obtain a copy of the License at
+#  *
+#  *      http://www.apache.org/licenses/LICENSE-2.0
+#  *
+#  * Unless required by applicable law or agreed to in writing, software
+#  * distributed under the License is distributed on an "AS IS" BASIS,
+#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  * See the License for the specific language governing permissions and
+#  * limitations under the License.
+#  * #L%
+#
+
 
 docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
 cd ../docker; ./build.sh $DOCKER_IMAGE_COORDINATES $TARGET_ENV
