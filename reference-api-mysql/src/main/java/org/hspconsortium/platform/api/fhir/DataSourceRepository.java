a4d9a80bbbeedd6d0cdc741b9d63156bee1f507a
Jacob Crump <jacob@interopion.com>
2019-02-14 11:13:56 -0700

    Further upgrades to springboot 2.0 and adding header licenses to all files.
    Former-commit-id: 9032fe10949577f7ca26917746a94d42221aa2a8

--- reference-api-mysql/src/main/java/org/hspconsortium/platform/api/fhir/DataSourceRepository.java
+++ reference-api-mysql/src/main/java/org/hspconsortium/platform/api/fhir/DataSourceRepository.java
@@ -1,16 +1,35 @@
+/**
+ *  * #%L
+ *  *
+ *  * %%
+ *  * Copyright (C) 2014-2019 Healthcare Services Platform Consortium
+ *  * %%
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *  * #L%
+ */
+
 package org.hspconsortium.platform.api.fhir;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
-import com.google.common.cache.CacheBuilder;
 import org.hspconsortium.platform.api.fhir.model.DataSet;
 import org.hspconsortium.platform.api.fhir.model.Sandbox;
 import org.hspconsortium.platform.api.fhir.service.SandboxService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.cache.caffeine.CaffeineCache;
 import com.github.benmanes.caffeine.cache.Cache;
 import org.springframework.stereotype.Component;
