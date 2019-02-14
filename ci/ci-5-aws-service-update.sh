93acbd0579b10017fbe7cfaa9bb85ea2aab7f288
Jacob Crump <jacob@interopion.com>
2019-02-14 11:13:56 -0700

    Further upgrades to springboot 2.0 and adding header licenses to all files.
    Former-commit-id: 12eba0b41241beca5e5a3f743f6dbe9481310642 [formerly c25f2ca76b280302fda5d3a7e8dce85422fde0bc]
    Former-commit-id: ab73b27e9d2ee88dbe905c6e96714d9aa9adce32

--- ci/ci-5-aws-service-update.sh
+++ ci/ci-5-aws-service-update.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env bash
+#
+#  * #%L
+#  *
+#  * %%
+#  * Copyright (C) 2014-2019 Healthcare Services Platform Consortium
+#  * %%
+#  * Licensed under the Apache License, Version 2.0 (the "License");
+#  * you may not use this file except in compliance with the License.
+#  * You may obtain a copy of the License at
+#  *
+#  *      http://www.apache.org/licenses/LICENSE-2.0
+#  *
+#  * Unless required by applicable law or agreed to in writing, software
+#  * distributed under the License is distributed on an "AS IS" BASIS,
+#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  * See the License for the specific language governing permissions and
+#  * limitations under the License.
+#  * #L%
+#
+
 
 # get the most recent task definition (assumes the task definition was just added)
 export AWS_TASK_DEFINITION=$(aws ecs describe-task-definition --region us-west-2 --task-definition ${PROJECT_FULL_NAME})
