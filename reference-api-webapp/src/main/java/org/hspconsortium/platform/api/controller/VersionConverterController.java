27bac1728fe70589d731a83c9f7e64ab082e21a4
Jacob Crump <jacob@interopion.com>
2019-02-21 09:36:58 -0700

    Updating empty schema and continued work on the converter.
    Former-commit-id: b6b63eb405a9923318df3308e68c064719a9f056

--- reference-api-webapp/src/main/java/org/hspconsortium/platform/api/controller/VersionConverterController.java
+++ reference-api-webapp/src/main/java/org/hspconsortium/platform/api/controller/VersionConverterController.java
@@ -1,14 +1,24 @@
 package org.hspconsortium.platform.api.controller;
 
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.model.api.IResource;
+import ca.uhn.fhir.parser.IParser;
 import org.hl7.fhir.convertors.VersionConvertor_30_40;
+import org.hl7.fhir.dstu3.model.Observation;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,43 +27,57 @@ import java.util.List;
 @RequestMapping("/converter")
 public class VersionConverterController {
 
-    @Value("${server.localhost}")
-    private String localhost;
+//    @Value("${server.localhost}")
+//    private String localhost;
 
-    @RequestMapping(method = RequestMethod.GET)
+    private static final Logger logger = LoggerFactory.getLogger(VersionConverterController.class);
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    private FhirContext ourFhirCtx = FhirContext.forDstu3();
+    private IParser parser = ourFhirCtx.newJsonParser().setPrettyPrint(true);
+    private VersionConvertor_30_40 converter = new VersionConvertor_30_40();
+    private String masterStu3Url = "http://localhost:8076/MasterStu3Smart/open/";
+    private String masterR4Url = "http://localhost:8076/MasterR4Smart/open/";
+
+    @RequestMapping(path = "", method = RequestMethod.GET)
     public void convertStarterSetToR4() {
-        VersionConvertor_30_40 converter = new VersionConvertor_30_40();
+
         String[] resources = {"AllergyIntolerance", "Binary", "Condition", "DocumentReference", "Encounter", "FamilyMemberHistory",
                 "ImagingStudy", "Immunization", "List", "MedicationDispense", "MedicationRequest", "Observation", "Patient", "Practitioner", "Procedure"};
         List<String> resourceList = Arrays.asList(resources);
-        String masterStu3Url = localhost + "/MasterStu3Smart/open/";
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity entity = new HttpEntity(headers);
+        JSONParser jsonParser = new JSONParser();
+
         for (String resourceName: resourceList) {
 
-            Boolean next = true;
+            Boolean next;
             String masterStu3UrlResource = masterStu3Url + resourceName + "?_count=50";
 
                 next = true;
                 while(next) {
-                    ResponseEntity<String> response = restTemplate.exchange(masterStu3UrlResource, HttpMethod.GET, entity, String.class);
+                    ResponseEntity<org.hl7.fhir.dstu3.model.Bundle> response = restTemplate.exchange(masterStu3UrlResource, HttpMethod.GET, entity, org.hl7.fhir.dstu3.model.Bundle.class);
                     if (response.hasBody()) {
-                        String jsonString = response.getBody();
+
                         try {
-                            JSONObject jsonBundle = (JSONObject) jsonParser.parse(jsonString);
-                            JSONArray linkArray = (JSONArray) jsonBundle.get("link");
+                            org.hl7.fhir.dstu3.model.Bundle bundle = response.getBody();
+                            ;
                             next = false;
-                            if (linkArray.size() >= 2) {
-                                for (int i = 0; i < linkArray.size(); i++) {
-                                    if (((JSONObject) linkArray.get(i)).get("relation").toString().equals("next")) {
+                            if (bundle.getLink().size() >= 2) {
+                                for (int i = 0; i < bundle.getLink().size(); i++) {
+                                    if (bundle.getLink().get(i).getRelation().equals("next")) {
                                         next = true;
-                                        masterStu3UrlResource = ((JSONObject) linkArray.get(i)).get("url").toString();
+                                        masterStu3UrlResource = bundle.getLink().get(i).getUrl();
                                     }
                                 }
                             }
 
-                            JSONArray entry = (JSONArray) jsonBundle.get("entry");
-                            for (int i = 0; i < entry.size(); i++) {
-                                JSONObject resource = (JSONObject) ((JSONObject) entry.get(i)).get("resource");
-
+                            ;
+                            for (int i = 0; i < bundle.getEntry().size(); i++) {
+                                String entry = bundle.getEntry().get(i).toString();
+                                convert(entry, resourceName);
                             }
                         } catch (Exception e) {
                             logger.error(e.getMessage());
@@ -65,7 +89,21 @@ public class VersionConverterController {
 
 
                 // Convert the resource
-        org.hl7.fhir.r4.model.Observation output = converter.convertObservation(input);
+
+    }
+
+    public void convert(String resourceString, String resourceName) {
+        if (resourceName.equals("Observation")) {
+            org.hl7.fhir.dstu3.model.Observation input = parser.parseResource(org.hl7.fhir.dstu3.model.Observation.class, resourceString);
+            org.hl7.fhir.r4.model.Observation output = converter.convertObservation(input);
+            org.hl7.fhir.r4.model.Observation output2 = output;
+        }
+        if (resourceName.equals("AllergyIntolerance")) {
+            org.hl7.fhir.dstu3.model.AllergyIntolerance input = parser.parseResource(org.hl7.fhir.dstu3.model.AllergyIntolerance.class, resourceString);
+            org.hl7.fhir.r4.model.AllergyIntolerance output = converter.convertAllergyIntolerance(input);
+            org.hl7.fhir.r4.model.AllergyIntolerance output2 = output;
+        }
+
     }
 
 }
