16f341c19dbf51a0a2320cf6a5159fde2810cb55
Jacob Crump <jacob@interopion.com>
2019-02-21 08:23:09 -0700

    Further work on converter.
    Former-commit-id: ff81716622a33e0fd1f20dcddc80bf861c3ccb23 [formerly 907c6cc04364f47ac84e87e8a85458de3618b9bf]
    Former-commit-id: aa9fc6ad09a92a6cd418487e5c99ff56f5a88f42

--- reference-api-webapp/src/main/java/org/hspconsortium/platform/api/controller/VersionConverterController.java
+++ reference-api-webapp/src/main/java/org/hspconsortium/platform/api/controller/VersionConverterController.java
@@ -1,22 +1,71 @@
 package org.hspconsortium.platform.api.controller;
 
 import org.hl7.fhir.convertors.VersionConvertor_30_40;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+@RestController
+@RequestMapping("/converter")
 public class VersionConverterController {
 
-    VersionConvertor_30_40 converter = new VersionConvertor_30_40();
-    String[] resources = {"AllergyIntolerance", "Binary", "Condition", "DocumentReference", "Encounter", "FamilyMemberHistory",
-            "ImagingStudy", "Immunization", "List", "MedicationDispense", "MedicationRequest", "Observation", "Patient", "Practitioner", "Procedure"};
-    List<String> resourceList = Arrays.asList(resources);
-    for (String resourceName: resourceList) {
+    @Value("${server.localhost}")
+    private String localhost;
 
+    @RequestMapping(method = RequestMethod.GET)
+    public void convertStarterSetToR4() {
+        VersionConvertor_30_40 converter = new VersionConvertor_30_40();
+        String[] resources = {"AllergyIntolerance", "Binary", "Condition", "DocumentReference", "Encounter", "FamilyMemberHistory",
+                "ImagingStudy", "Immunization", "List", "MedicationDispense", "MedicationRequest", "Observation", "Patient", "Practitioner", "Procedure"};
+        List<String> resourceList = Arrays.asList(resources);
+        String masterStu3Url = localhost + "/MasterStu3Smart/open/";
+        for (String resourceName: resourceList) {
+
+            Boolean next = true;
+            String masterStu3UrlResource = masterStu3Url + resourceName + "?_count=50";
+
+                next = true;
+                while(next) {
+                    ResponseEntity<String> response = restTemplate.exchange(masterStu3UrlResource, HttpMethod.GET, entity, String.class);
+                    if (response.hasBody()) {
+                        String jsonString = response.getBody();
+                        try {
+                            JSONObject jsonBundle = (JSONObject) jsonParser.parse(jsonString);
+                            JSONArray linkArray = (JSONArray) jsonBundle.get("link");
+                            next = false;
+                            if (linkArray.size() >= 2) {
+                                for (int i = 0; i < linkArray.size(); i++) {
+                                    if (((JSONObject) linkArray.get(i)).get("relation").toString().equals("next")) {
+                                        next = true;
+                                        masterStu3UrlResource = ((JSONObject) linkArray.get(i)).get("url").toString();
+                                    }
+                                }
+                            }
+
+                            JSONArray entry = (JSONArray) jsonBundle.get("entry");
+                            for (int i = 0; i < entry.size(); i++) {
+                                JSONObject resource = (JSONObject) ((JSONObject) entry.get(i)).get("resource");
+
+                            }
+                        } catch (Exception e) {
+                            logger.error(e.getMessage());
+                        }
+                    }
+                }
+
+        }
+
+
+                // Convert the resource
+        org.hl7.fhir.r4.model.Observation output = converter.convertObservation(input);
     }
-
-
-            // Convert the resource
-    org.hl7.fhir.r4.model.Observation output = converter.convertObservation(input);
 
 }
