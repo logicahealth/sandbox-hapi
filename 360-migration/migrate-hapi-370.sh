34e50cfc3e2bb7e76198866556c3874c262318b1
Jacob Crump <jacob@interopion.com>
2019-02-22 17:21:03 -0700

    More work on migration.
    Former-commit-id: e14aef5c7b263ac0a3ff891f2146f97b90530610 [formerly 1a05abcb9eef0754a42235ea65c7dc66a6127d7d]
    Former-commit-id: 5ba84b7bcd6dde5bf513bf77cfe22310a345a9da

--- 360-migration/migrate-hapi-370.sh
+++ 360-migration/migrate-hapi-370.sh
@@ -41,29 +41,19 @@ case "${ENVIRONMENT}" in
         ;;
 esac
 
-#DB_STARTS_WITH="hspc_5"
-#
-#SQL_STRING="SELECT SCHEMA_NAME AS db FROM information_schema.SCHEMATA WHERE SCHEMA_NAME NOT IN ('mysql', 'information_schema') AND SCHEMA_NAME LIKE 'hspc_5%';"
-## Pipe the SQL into mysql
-#DBS=$(echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -Bs)
-#set -f                      # avoid globbing (expansion of *).
-#array=($(echo "$DBS" | tr ',' '\n'))
-## Display your result
-#for FULL_NAME in "${array[@]}"
-#do
-#    SANDBOX_NAME=${FULL_NAME:7}
-#	echo "$SANDBOX_NAME"
-#done
 SANDBOX_NAME=${FULL_NAME:7}
 case "$FHIR_VERSION" in
     dstu2)
         PORT="8075"
+        INDEX="8"
         ;;
     stu3)
         PORT="8076"
+        INDEX="9"
         ;;
     r4)
         PORT="8077"
+        INDEX="10"
         ;;
 esac
 FHIR_HOST="http://127.0.0.1:$PORT"
@@ -72,10 +62,10 @@ case "${ENVIRONMENT}" in
         FHIR_HOST="http://127.0.0.1:$PORT"
         ;;
     test)
-        FHIR_HOST="https://api-v5-$fhirVersion-test.hspconsortium.org"
+        FHIR_HOST="https://api-v5-$FHIR_VERSION-test.hspconsortium.org"
         ;;
     prod)
-        FHIR_HOST="https://api-v5-$fhirVersion.hspconsortium.org"
+        FHIR_HOST="https://api-v5-$FHIR_VERSION.hspconsortium.org"
         ;;
 esac
 
@@ -130,25 +120,8 @@ done
 
 hapi-fhir-3.7.0-cli/hapi-fhir-cli migrate-database -d MYSQL_5_7 -u "jdbc:mysql://$HOST/$FULL_NAME?serverTimezone=America/Denver" -n "$MYSQL_USER" -p "$MYSQL_PASS" -f V3_4_0 -t V3_7_0
 
-#declare -A my_dict
-#FOUND=0
-#IFS="$( echo -e '\t' )"
-#mysql -u$MYSQL_USER -p$MYSQL_PASS -e "SELECT * FROM $FULL_NAME.HFJ_SPIDX_STRING WHERE HASH_IDENTITY is null;" |
-#    while read SP_ID SP_MISSING SP_NAME RES_ID RES_TYPE SP_UPDATED SP_VALUE_EXACT SP_VALUE_NORMALIZED HASH_EXACT HASH_NORM_PREFIX HASH_IDENTITY; do
-#
-#    for key in ${!my_dict[@]}; do
-#        if [[ ${key} == $RES_TYPE && ${my_dict[${key}]} == *",$SP_NAME,"* ]]; then
-#            let FOUND=1
-#        fi
-#    done
-#
-#    if [[ $FOUND -eq 0 ]]; then
-#        my_dict[$RES_TYPE]+=",$SP_NAME,"
-#        HASH=$(curl --silent "http://localhost:8076/$SANDBOX_NAME/sandbox/hash/$RES_TYPE,$SP_NAME" --header "Authorization: BEARER ${BEARER_TOKEN}")
-#        mysql -u$MYSQL_USER -p$MYSQL_PASS -e "UPDATE $FULL_NAME.HFJ_SPIDX_STRING SET HASH_IDENTITY='$HASH' WHERE RES_TYPE='$RES_TYPE' AND SP_NAME='$SP_NAME';"
-#    fi
-#    let FOUND=0
-#done
+SQL_STRING="UPDATE sandman.sandbox SET api_endpoint_index='$INDEX' WHERE sandbox_id='$SANDBOX_NAME'";
+echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -Bs
 
 echo "Shutting down sever"
 ps ax | grep nameForShutdown | grep -v grep | awk '{print $1}' | xargs kill
