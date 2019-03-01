dcce656a8b5f0d0c29534807b0b9cc977bb9b905
Shilpy <shilpy@interopion.com>
2019-03-01 15:28:46 -0700

    Deleted the code to deleteSelectedSD.  Not needed.  SDs could be deleted from the front-end
    Former-commit-id: 1987a2952531200da3046ceb64abf246e0a92733

--- reference-api-mysql/src/main/java/org/hspconsortium/platform/api/fhir/service/ProfileService.java
+++ reference-api-mysql/src/main/java/org/hspconsortium/platform/api/fhir/service/ProfileService.java
@@ -10,5 +10,4 @@ import java.util.zip.ZipFile;
 public interface ProfileService {
     HashMap<List<String>, List<String>> saveZipFile (ZipFile zipFile, HttpServletRequest request, String sandboxId) throws IOException;
     HashMap<String, List<JSONObject>> getAllUploadedProfiles(HttpServletRequest request, String sandboxId);
-    String deleteSelectedSD(HttpServletRequest request, String sandboxId, String structureDefinitionID);
 }
