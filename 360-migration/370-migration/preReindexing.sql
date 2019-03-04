33a5bb5453764689985b0ba2afeddf3c4f620fd8
pz12 <jkcrump12@gmail.com>
2019-03-04 15:29:32 -0700

    Finishing things up.
    Former-commit-id: 2ccd466ea409595dad98ea95ef5366a05312b829 [formerly 93398b7a18dadc3c8c9a775a0e578f63cd744014] [formerly 974aca6e2b6b36ff4c222e59a196c3bc80c5a006 [formerly 59dd853879765b628b874ae9b7a1fa862477e9bc]]
    Former-commit-id: ce3400643ea945b3f9df6a8b92564eb5da78be9a
    Former-commit-id: 0f4abc296d8ef5952bf36b23daf870111ba6b502 [formerly 2501304c0a8dcb6ea97a2a1296ece6a5375e6d29]
    Former-commit-id: f5742f524cc2db2c8b7d52f13a339ca3c0ec652b

--- 360-migration/370-migration/preReindexing.sql
+++ 360-migration/370-migration/preReindexing.sql
@@ -18,6 +18,18 @@
 --  * #L%
 --
 
+DELIMITER $$
+DROP PROCEDURE IF EXISTS drop_index_if_exists $$
+CREATE PROCEDURE drop_index_if_exists(in theTable varchar(128), in theIndexName varchar(128))
+BEGIN
+ IF((SELECT COUNT(*) AS index_exists FROM information_schema.statistics WHERE TABLE_SCHEMA = DATABASE() and table_name =
+theTable AND index_name = theIndexName) > 0) THEN
+   SET @s = CONCAT('DROP INDEX ' , theIndexName , ' ON ' , theTable);
+   PREPARE stmt FROM @s;
+   EXECUTE stmt;
+ END IF;
+END $$
+
 DELIMITER ^^
 
 DROP PROCEDURE IF EXISTS create_index_if_not_exists ^^
