f8ad1a955c49f76be0a1891072b052426cb356f4
pz12 <jkcrump12@gmail.com>
2019-03-04 14:18:37 -0700

    Merge branch 'develop' into feature/hapi-360
    # Conflicts:
    #	360-migration/postReindexing.sql
    #	360-migration/preReindexing.sql
    #	360-migration/run-fhir-server.sh
    #	pom.xml
    #	reference-api-mysql/pom.xml
    #	reference-api-mysql/src/main/java/org/hspconsortium/platform/api/fhir/config/HapiJpaConfigStu3.java
    #	reference-api-mysql/src/main/resources/db/mysql/hspc_5_schema_empty.sql
    #	reference-api-oauth2/pom.xml
    #	reference-api-smart-support/pom.xml
    #	reference-api-webapp/pom.xml
    #	reference-api-webapp/src/main/resources/application.yml
    
    
    Former-commit-id: 58aed1afef2a73526102e753660c20c6843c7601 [formerly 4aa621da00a3517b229f09a182bf9bd4279c706f]
    Former-commit-id: 662de25bf2fdad8bdf1dfb2aa0375663fd963bf7

---
+++ reference-api-webapp/src/main/java/org/hspconsortium/platform/api/service/ProfileServiceImpl.java
@@ -0,0 +1,203 @@
+package org.hspconsortium.platform.api.service;
+
+import org.hspconsortium.platform.api.fhir.service.ProfileService;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.util.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+
+
+@Component
+public class ProfileServiceImpl implements ProfileService {
+    private static final Logger logger = LoggerFactory.getLogger(SandboxServiceImpl.class);
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${server.localhost}")
+    private String localhost;
+
+    @Value("${hspc.platform.api.fhir.profileResources}")
+    private String[] profileResources;
+
+    public HashMap<String, List<JSONObject>> getAllUploadedProfiles(HttpServletRequest request, String sandboxId) {
+        String authToken = request.getHeader("Authorization").substring(7);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "BEARER " + authToken);
+        HttpEntity entity = new HttpEntity(headers);
+
+        HashMap<String, List<JSONObject>> urlAndResources = new HashMap<>();
+
+        String url = "";
+        String completeUrl = "";
+        String profileUrl = "";
+        String currentUrl = "";
+
+        JSONParser jsonParser = new JSONParser();
+        List<JSONObject> urtList = new ArrayList<>();
+        Boolean next = true;
+
+        for (String resourceType: profileResources) {
+            url = localhost + "/" + sandboxId + "/data/" + resourceType + "?_count=50";
+            next = true;
+            while(next) {
+                ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+                if (response.hasBody()) {
+                    String jsonString = response.getBody();
+                    try {
+                        JSONObject jsonBundle = (JSONObject) jsonParser.parse(jsonString);
+                        JSONArray  linkArray = (JSONArray) jsonBundle.get("link");
+                        next = false;
+                        if (linkArray.size() >= 2) {
+                            for (int i = 0; i < linkArray.size(); i++) {
+                                if (((JSONObject) linkArray.get(i)).get("relation").toString().equals("next")) {
+                                    next = true;
+                                    url = ((JSONObject) linkArray.get(i)).get("url").toString();
+                                }
+                            }
+                        }
+
+                        JSONArray entry = (JSONArray) jsonBundle.get("entry");
+                        for (int i = 0; i < entry.size(); i++) {
+                            JSONObject resource = (JSONObject) ((JSONObject) entry.get(i)).get("resource");
+                            completeUrl = resource.get("url").toString();
+                            if (completeUrl.contains(resourceType)) {
+                                profileUrl = completeUrl.substring(0, completeUrl.indexOf(resourceType));
+                            } else {
+                                profileUrl = completeUrl;
+                            }
+                            if (currentUrl.isEmpty()) {
+                                currentUrl = profileUrl;
+                                urtList.add(resource);
+                            } else if (profileUrl.equals(currentUrl)) {
+                                urtList.add(resource);
+                            } else {
+                                urlAndResources.put(currentUrl, urtList);
+                                currentUrl = profileUrl;
+                                urtList.clear();
+                                urtList.add(resource);
+                                urlAndResources.put(currentUrl, urtList);
+                                //TODO: not adding SD and another CS if the current url has changed.
+                                // it only added one resource to the QICore, where it should be adding 3
+                            }
+                        }
+                    } catch (Exception e) {
+                        logger.error(e.getMessage());
+                    }
+                }
+            }
+        }
+        return urlAndResources;
+    }
+
+    public HashMap<List<String>, List<String>> saveZipFile (ZipFile zipFile, HttpServletRequest request, String sandboxId, String apiEndpoint) throws IOException {
+        HashMap<List<String>, List<String>> successAndFailureList = new HashMap<>();
+        String authToken = request.getHeader("Authorization").substring(7);
+        List<String> resourceSaved = new ArrayList<>();
+        List<String> resourceNotSaved = new ArrayList<>();
+        Enumeration zipFileEntries = zipFile.entries();
+        while(zipFileEntries.hasMoreElements()) {
+            ZipEntry entry = (ZipEntry) zipFileEntries.nextElement();
+            String fileName = entry.getName();
+            if (fileName.endsWith(".json")) {
+                InputStream inputStream = zipFile.getInputStream(entry);
+                JSONParser jsonParser = new JSONParser();
+                try {
+                    JSONObject jsonObject = (JSONObject)jsonParser.parse(new InputStreamReader(inputStream, "UTF-8"));
+                    String resourceType = jsonObject.get("resourceType").toString();
+                    String resourceName = jsonObject.get("name").toString();
+                    if (resourceType.equals("StructureDefinition")) {
+                        String fhirVersion = jsonObject.get("fhirVersion").toString();
+                        if (apiEndpoint.equals("5") && !fhirVersion.equals("1.0.2")) {
+                            throw new RuntimeException(fileName + " FHIR version is incompatible with your current sandbox's FHIR version. The profile was not saved.");
+                        } else if (apiEndpoint.equals("6")&& !fhirVersion.equals("3.0.1")) {
+                            throw new RuntimeException(fileName + " FHIR version is incompatible with your current sandbox's FHIR version. The profile was not saved.");
+                        } else if (apiEndpoint.equals("7") && !fhirVersion.equals("4.0.0")) {
+                            throw new RuntimeException(fileName + " FHIR version is incompatible with your current sandbox's FHIR version. The profile was not saved.");
+                        }
+                    }
+                    if (Arrays.stream(profileResources).anyMatch(resourceType::equals)) {
+                        String jsonBody = jsonObject.toString();
+                        HttpHeaders headers = new HttpHeaders();
+                        headers.set("Authorization", "BEARER " + authToken);
+                        headers.set("Content-Type", "application/json");
+                        String url = localhost + "/" + sandboxId + "/data/" + resourceType + "/" + jsonObject.get("id").toString();
+                        HttpEntity entity = new HttpEntity(jsonBody, headers);
+                        try {
+                            restTemplate.exchange(url, HttpMethod.PUT, entity, String.class);
+                            resourceSaved.add(resourceType + " - " + resourceName);
+                        } catch (HttpServerErrorException | HttpClientErrorException e) {
+                            resourceNotSaved.add(resourceType + " - " + resourceName);
+                        }
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(e.getMessage());
+                }
+            }
+        }
+        successAndFailureList.put(resourceSaved, resourceNotSaved);
+        return successAndFailureList;
+    }
+}
+
+//            if (fileName.endsWith(".xml")) {
+//                beginsWith = fileName.substring(0, fileName.indexOf("-"));
+//                if(beginsWith.equals("StructureDefinition") || (beginsWith.equals("ValueSet")) || (beginsWith.equals("CodeSystem"))) {
+//                    InputStream inputStream = zipFile.getInputStream(entry);
+//
+//                    try {
+//                        BufferedReader inputReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+//                        StringBuilder stringBuilder = new StringBuilder();
+//                        String inline = "";
+//                        while ((inline = inputReader.readLine()) != null) {
+//                            stringBuilder.append(inline);
+//                        }
+//                        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+//                        DocumentBuilder builder = factory.newDocumentBuilder();
+//                        InputSource source = new InputSource();
+//                        source.setCharacterStream(new StringReader(stringBuilder.toString()));
+//
+//                        Document doc = builder.parse(source);
+//
+////                        Document doc = builder.parse(new InputSource (new ByteArrayInputStream(stringBuilder.toString().getBytes(StandardCharsets.UTF_8))));
+////                        InputStream inputStream2 = new    ByteArrayInputStream(stringBuilder.toString().getBytes(StandardCharsets.UTF_8));
+////                        org.w3c.dom.Document doc = builder.parse(inputStream2);
+//
+//                        Element element = doc.getDocumentElement();
+//                        String id = element.getAttribute("id value");
+//
+//                        HttpHeaders headers = new HttpHeaders();
+//                        headers.set("Authorization", "BEARER " + authToken);
+//                        headers.set("Content-Type", "application/xml");
+//
+//                        String url = localhost + "/" + sandboxId + "/data/" +  beginsWith + "/" + element.getAttribute("id value");
+//                        HttpEntity entity = new HttpEntity(stringBuilder.toString(), headers);
+//                        try {
+//                            restTemplate.exchange(url, HttpMethod.PUT, entity, String.class);
+//                        } catch (HttpClientErrorException e) {
+//                            logger.error("File not saved: " + fileName);
+//                        }
+//                    } catch (Exception e) {
+//                        logger.error(e.getMessage() + "Unsupported file " + fileName);
+//                    }
+//                }
+//            }
+
+
+
