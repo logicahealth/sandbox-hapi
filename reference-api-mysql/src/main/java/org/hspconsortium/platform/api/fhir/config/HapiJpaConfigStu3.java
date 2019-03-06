93decba8511195d14ee8bfbb298506c7cb7433e1
Jacob Crump <jacob@interopion.com>
2019-03-06 14:49:48 -0700

    Changing stuff for 370 migration.
    Former-commit-id: 50e945c508c9e99034281cfbb9e7294d96e97457 [formerly bd09fe7a3cba0ed03c97361296f19eb674ccd867]
    Former-commit-id: 4409774f2363c66d557e23823f2cd3d62c8f9f67

--- reference-api-mysql/src/main/java/org/hspconsortium/platform/api/fhir/config/HapiJpaConfigStu3.java
+++ reference-api-mysql/src/main/java/org/hspconsortium/platform/api/fhir/config/HapiJpaConfigStu3.java
@@ -21,15 +21,22 @@
 package org.hspconsortium.platform.api.fhir.config;
 
 import ca.uhn.fhir.jpa.config.BaseJavaConfigDstu3;
+import ca.uhn.fhir.jpa.dao.DaoMethodOutcome;
+import ca.uhn.fhir.jpa.dao.IFhirResourceDaoValueSet;
+import ca.uhn.fhir.jpa.dao.dstu3.FhirResourceDaoValueSetDstu3;
 import ca.uhn.fhir.jpa.search.ISearchCoordinatorSvc;
 import ca.uhn.fhir.jpa.util.SubscriptionsRequireManualActivationInterceptorDstu3;
+import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.server.interceptor.IServerInterceptor;
+import org.hl7.fhir.dstu3.model.CodeableConcept;
+import org.hl7.fhir.dstu3.model.Coding;
+import org.hl7.fhir.dstu3.model.ValueSet;
 import org.hspconsortium.platform.api.fhir.multitenant.search.MultiTenantSearchCoordinatorSvcImpl;
 import org.springframework.beans.factory.annotation.Autowire;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @Profile("stu3")
 @Configuration
@@ -45,4 +52,45 @@ public class HapiJpaConfigStu3 extends BaseJavaConfigDstu3 {
         return new MultiTenantSearchCoordinatorSvcImpl();
     }
 
+    @Bean(name = "myValueSetDaoDstu3", autowire = Autowire.BY_NAME)
+    @Lazy
+    public IFhirResourceDaoValueSet<ValueSet, Coding, CodeableConcept> daoValueSetDstu3() {
+        MyFhirResourceDaoValueSetDstu3 retVal = new MyFhirResourceDaoValueSetDstu3();
+        retVal.setResourceType(org.hl7.fhir.dstu3.model.ValueSet.class);
+        retVal.setContext(fhirContextDstu3());
+        return retVal;
+    }
+
+    public static class MyFhirResourceDaoValueSetDstu3 extends FhirResourceDaoValueSetDstu3 {
+        @Autowired
+        private PlatformTransactionManager myTxManager;
+
+        @Override
+        public DaoMethodOutcome create(ValueSet theResource, RequestDetails theRequestDetails) {
+            return new TransactionTemplate(myTxManager).execute(t -> {
+                return super.create(theResource, theRequestDetails);
+            });
+        }
+
+        @Override
+        public DaoMethodOutcome create(ValueSet theResource, String theIfNoneExist, RequestDetails theRequestDetails) {
+            return new TransactionTemplate(myTxManager).execute(t -> {
+                return super.create(theResource, theIfNoneExist, theRequestDetails);
+            });
+        }
+
+        @Override
+        public DaoMethodOutcome update(ValueSet theResource, RequestDetails theRequestDetails) {
+            return new TransactionTemplate(myTxManager).execute(t -> {
+                return super.update(theResource, theRequestDetails);
+            });
+        }
+
+        @Override
+        public DaoMethodOutcome update(ValueSet theResource, String theIfNoneExist, RequestDetails theRequestDetails) {
+            return new TransactionTemplate(myTxManager).execute(t -> {
+                return super.update(theResource, theIfNoneExist, theRequestDetails);
+            });
+        }
+    }
 }
