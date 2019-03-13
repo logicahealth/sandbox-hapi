cda1890246b7db21642bdde333c4d9132852d11a
Jacob Crump <jacob@interopion.com>
2019-03-12 19:06:27 -0600

    Fixing migration script.
    Former-commit-id: 4eb7cbe503ecf5a2ee79f9ba9f91682a4894c743 [formerly 70ad370c6e723e2b9366a81f6a211165ca4da7ee] [formerly e649b1548019ea87363e9cb8275c09d43835b0db [formerly 690013628f8ebd1e990d1dd55a1869e317df5bff]]
    Former-commit-id: f9b35c514a33d783640f012dc335dbf04989114b
    Former-commit-id: 10baa7e3710d478b3845a51151d939aff8b4e74d [formerly b25bd5f47fa963843c2ea356318aae6c2addff54]
    Former-commit-id: fa6826183a9276ba3a9915424dc99b3bf01d4ff9

--- 360-migration/370-migration/all-sandboxes-to-370.sh
+++ 360-migration/370-migration/all-sandboxes-to-370.sh
@@ -50,130 +50,94 @@ while true; do
 
     # Display your result
     FULL_NAME="${array[$(( ( RANDOM % ${#array[@]} ) ))]}"
-
+#    FULL_NAME="hspc_5_SSSTU31108"
 
     SANDBOX_NAME=${FULL_NAME:7}
     SQL_STRING="SHOW DATABASES LIKE '$TEMP_SCHEMA$SANDBOX_NAME';"
     if [[ -z "$(echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs)" ]]; then
 
         STARTED=0
-        SQL_STRING="SELECT RES_VERSION FROM $FULL_NAME.HFJ_RESOURCE WHERE RES_ID=1;"
+        SQL_STRING="SELECT api_endpoint_index FROM sandman.sandbox WHERE sandbox_id='$SANDBOX_NAME';"
         # Pipe the SQL into mysql
         FHIR_VERSION=$(echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs)
         case "${FHIR_VERSION}" in
-            DSTU2)
+            5)
                 FHIR_VERSION="dstu2"
                 ;;
-            DSTU3)
+            6)
                 FHIR_VERSION="stu3"
                 ;;
-            R4)
+            7)
                 FHIR_VERSION="r4"
                 ;;
         esac
         if [[ $FHIR_VERSION != "r4" && $FULL_NAME != 'hspc_5_fhirnpi' && $FULL_NAME != 'hspc_5_hspc5' && $FULL_NAME != 'hspc_5_hspc6' && $FULL_NAME != 'hspc_5_hspc7' ]]; then
+
+            echo "USE $FULL_NAME" | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
+            SQL_STRING="UPDATE sandman.sandbox SET expiration_message='For the next several minutes, your sandbox will be undergoing an upgrade. Any changes made to the FHIR data of this sandbox may not be saved while this banner is showing.', expiration_date='2019-04-01' WHERE sandbox_id='$SANDBOX_NAME';"
+            echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
+            SQL_STRING="DROP DATABASE IF EXISTS $TEMP_SCHEMA$SANDBOX_NAME;"
+            echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
+            SQL_STRING="CREATE SCHEMA $TEMP_SCHEMA$SANDBOX_NAME;"
+            echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
+            echo "$SANDBOX_NAME" >> sandboxes_done.txt
+            mysqldump --host=$HOST --protocol=tcp --user=$MYSQL_USER --password=$MYSQL_PASS --hex-blob=TRUE --port=3306 --default-character-set=utf8 --skip-triggers "$FULL_NAME" > temp-dump.sql
+
+            sed -i -e 's/trm_concept_desig/TRM_CONCEPT_DESIG/g' temp-dump.sql
+            sed -i -e 's/trm_concept_map/TRM_CONCEPT_MAP/g' temp-dump.sql
+            sed -i -e 's/TRM_CONCEPT_MAP_group/TRM_CONCEPT_MAP_GROUP/g' temp-dump.sql
+            sed -i -e 's/TRM_CONCEPT_MAP_grp_element/TRM_CONCEPT_MAP_GRP_ELEMENT/g' temp-dump.sql
+            sed -i -e 's/TRM_CONCEPT_MAP_grp_elm_tgt/TRM_CONCEPT_MAP_GRP_ELM_TGT/g' temp-dump.sql
+
+            mysql --user=$MYSQL_USER --password=$MYSQL_PASS --host=$HOST --port=3306 "$TEMP_SCHEMA$SANDBOX_NAME" < temp-dump.sql
+
+            echo "Running Pre-Reindexing sql scripts for $SANDBOX_NAME"
+            mysql --user=$MYSQL_USER --password=$MYSQL_PASS --host=$HOST --port=3306 "$TEMP_SCHEMA$SANDBOX_NAME" < preReindexing.sql
 
             SQL_STRING="SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA ='$FULL_NAME' and TABLE_NAME='HFJ_SPIDX_TOKEN' and COLUMN_NAME='HASH_IDENTITY'"
             SQL_STRING2="SELECT COUNT(*) FROM $FULL_NAME.HFJ_RESOURCE;"
             if [[ "$(echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs)" != "0" ]]; then
                 SQL_STRING3="SELECT COUNT(*) FROM $FULL_NAME.HFJ_SPIDX_TOKEN WHERE HASH_IDENTITY IS NULL;"
                 if [[ "$(echo $SQL_STRING3 | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs)" != "0" ]]; then
-                    SQL_STRING="UPDATE sandman.sandbox SET expiration_message='For the next several minutes, your sandbox will be undergoing an upgrade. Any changes made to the FHIR data of this sandbox may not be saved while this banner is showing.', expiration_date='2019-04-01' WHERE sandbox_id='$SANDBOX_NAME';"
-                    echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
-                    SQL_STRING="DROP DATABASE IF EXISTS $TEMP_SCHEMA$SANDBOX_NAME;"
-                    echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
-                    SQL_STRING="CREATE SCHEMA $TEMP_SCHEMA$SANDBOX_NAME;"
-                    echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
-
-                    mysqldump --host=$HOST --protocol=tcp --user=$MYSQL_USER --password=$MYSQL_PASS --hex-blob=TRUE --port=3306 --default-character-set=utf8 --skip-triggers "$FULL_NAME" > temp-dump.sql
-                    mysql --user=$MYSQL_USER --password=$MYSQL_PASS --host=$HOST --port=3306 "$TEMP_SCHEMA$SANDBOX_NAME" < temp-dump.sql
-                    echo "Running Pre-Reindexing sql scripts for $SANDBOX_NAME"
-                    mysql --user=$MYSQL_USER --password=$MYSQL_PASS --host=$HOST --port=3306 "$TEMP_SCHEMA$SANDBOX_NAME" < preReindexing.sql
                    ./migrate-hapi-370.sh $MYSQL_USER $MYSQL_PASS $ENVIRONMENT "$TEMP_SCHEMA$SANDBOX_NAME" $FHIR_VERSION $BEARER_TOKEN $JASYPT_PASSWORD
 
-                   mysqldump --host=$HOST --protocol=tcp --user=$MYSQL_USER --password=$MYSQL_PASS --hex-blob=TRUE --port=3306 --default-character-set=utf8 --skip-triggers "$TEMP_SCHEMA$SANDBOX_NAME" > temp-dump.sql
-                    SQL_STRING="DROP DATABASE $FULL_NAME; CREATE SCHEMA hspc_8_$SANDBOX_NAME;"
-                    echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
-
-                    mysql --user=$MYSQL_USER --password=$MYSQL_PASS --host=$HOST --port=3306 "hspc_8_$SANDBOX_NAME" < temp-dump.sql
-
-                    SQL_STRING="UPDATE hspc_8_$SANDBOX_NAME.hspc_tenant_info SET hspc_schema_version='8' WHERE tenant_id='$SANDBOX_NAME';"
-                    echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
-
-                    SQL_STRING="SELECT api_endpoint_index FROM sandman.sandbox WHERE sandbox_id='$SANDBOX_NAME';"
-                    FHIR_VERSION_NUMBER=$(echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs)
-                    case "${FHIR_VERSION_NUMBER}" in
-                    5)
-                        FHIR_VERSION_NUMBER="8"
-                        ;;
-                    6)
-                        FHIR_VERSION_NUMBER="9"
-                        ;;
-                    7)
-                        FHIR_VERSION_NUMBER="10"
-                        ;;
-                    esac
-
-                    SQL_STRING="UPDATE sandman.sandbox SET api_endpoint_index='$FHIR_VERSION_NUMBER' WHERE sandbox_id='$SANDBOX_NAME';"
-                    echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
-
-                    SQL_STRING="DROP DATABASE $TEMP_SCHEMA$SANDBOX_NAME;"
-                    echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
-
-                   SQL_STRING="UPDATE sandman.sandbox SET expiration_message=NULL, expiration_date=NULL WHERE sandbox_id='$SANDBOX_NAME';"
-                    echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
-                    echo "$SANDBOX_NAME" >> sandboxes_done.txt
                 fi
             elif [[ "$(echo $SQL_STRING2 | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs)" != "0" ]]; then
-                echo "USE $FULL_NAME" | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
-                SQL_STRING="UPDATE sandman.sandbox SET expiration_message='For the next several minutes, your sandbox will be undergoing an upgrade. Any changes made to the FHIR data of this sandbox may not be saved while this banner is showing.', expiration_date='2019-04-01' WHERE sandbox_id='$SANDBOX_NAME';"
-                echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
-                SQL_STRING="DROP DATABASE IF EXISTS $TEMP_SCHEMA$SANDBOX_NAME;"
-                echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
-                SQL_STRING="CREATE SCHEMA $TEMP_SCHEMA$SANDBOX_NAME;"
-                echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
-                echo "$SANDBOX_NAME" >> sandboxes_done.txt
-                mysqldump --host=$HOST --protocol=tcp --user=$MYSQL_USER --password=$MYSQL_PASS --hex-blob=TRUE --port=3306 --default-character-set=utf8 --skip-triggers "$FULL_NAME" > temp-dump.sql
-                mysql --user=$MYSQL_USER --password=$MYSQL_PASS --host=$HOST --port=3306 "$TEMP_SCHEMA$SANDBOX_NAME" < temp-dump.sql
-
-                echo "Running Pre-Reindexing sql scripts for $SANDBOX_NAME"
-                mysql --user=$MYSQL_USER --password=$MYSQL_PASS --host=$HOST --port=3306 "$TEMP_SCHEMA$SANDBOX_NAME" < preReindexing.sql
                 ./migrate-hapi-370.sh $MYSQL_USER $MYSQL_PASS $ENVIRONMENT "$TEMP_SCHEMA$SANDBOX_NAME" $FHIR_VERSION $BEARER_TOKEN $JASYPT_PASSWORD
-
-                mysqldump --host=$HOST --protocol=tcp --user=$MYSQL_USER --password=$MYSQL_PASS --hex-blob=TRUE --port=3306 --default-character-set=utf8 --skip-triggers "$TEMP_SCHEMA$SANDBOX_NAME" > temp-dump.sql
-                SQL_STRING="DROP DATABASE $FULL_NAME; CREATE SCHEMA hspc_8_$SANDBOX_NAME;"
-                echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
-
-                mysql --user=$MYSQL_USER --password=$MYSQL_PASS --host=$HOST --port=3306 "hspc_8_$SANDBOX_NAME" < temp-dump.sql
-
-                SQL_STRING="UPDATE hspc_8_$SANDBOX_NAME.hspc_tenant_info SET hspc_schema_version='8' WHERE tenant_id='$SANDBOX_NAME';"
-                echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
-
-                SQL_STRING="SELECT api_endpoint_index FROM sandman.sandbox WHERE sandbox_id='$SANDBOX_NAME';"
-                FHIR_VERSION_NUMBER=$(echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs)
-                case "${FHIR_VERSION_NUMBER}" in
-                5)
-                    FHIR_VERSION_NUMBER="8"
-                    ;;
-                6)
-                    FHIR_VERSION_NUMBER="9"
-                    ;;
-                7)
-                    FHIR_VERSION_NUMBER="10"
-                    ;;
-                esac
-
-                SQL_STRING="UPDATE sandman.sandbox SET api_endpoint_index='$FHIR_VERSION_NUMBER' WHERE sandbox_id='$SANDBOX_NAME';"
-                echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
-
-                SQL_STRING="DROP DATABASE $TEMP_SCHEMA$SANDBOX_NAME;"
-                echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
-
-                SQL_STRING="UPDATE sandman.sandbox SET expiration_message=NULL, expiration_date=NULL WHERE sandbox_id='$SANDBOX_NAME';"
-                echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
-                echo "$SANDBOX_NAME" >> sandboxes_done.txt
             fi
 
+            mysqldump --host=$HOST --protocol=tcp --user=$MYSQL_USER --password=$MYSQL_PASS --hex-blob=TRUE --port=3306 --default-character-set=utf8 --skip-triggers "$TEMP_SCHEMA$SANDBOX_NAME" > temp-dump.sql
+            SQL_STRING="DROP DATABASE $FULL_NAME; CREATE SCHEMA hspc_8_$SANDBOX_NAME;"
+            echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
+
+            mysql --user=$MYSQL_USER --password=$MYSQL_PASS --host=$HOST --port=3306 "hspc_8_$SANDBOX_NAME" < temp-dump.sql
+
+            SQL_STRING="UPDATE hspc_8_$SANDBOX_NAME.hspc_tenant_info SET hspc_schema_version='8' WHERE tenant_id='$SANDBOX_NAME';"
+            echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
+
+            SQL_STRING="SELECT api_endpoint_index FROM sandman.sandbox WHERE sandbox_id='$SANDBOX_NAME';"
+            FHIR_VERSION_NUMBER=$(echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs)
+            case "${FHIR_VERSION_NUMBER}" in
+            5)
+                FHIR_VERSION_NUMBER="8"
+                ;;
+            6)
+                FHIR_VERSION_NUMBER="9"
+                ;;
+            7)
+                FHIR_VERSION_NUMBER="10"
+                ;;
+            esac
+
+            SQL_STRING="UPDATE sandman.sandbox SET api_endpoint_index='$FHIR_VERSION_NUMBER' WHERE sandbox_id='$SANDBOX_NAME';"
+            echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
+
+            SQL_STRING="DROP DATABASE $TEMP_SCHEMA$SANDBOX_NAME;"
+            echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
+
+            SQL_STRING="UPDATE sandman.sandbox SET expiration_message=NULL, expiration_date=NULL WHERE sandbox_id='$SANDBOX_NAME';"
+            echo $SQL_STRING | mysql -u$MYSQL_USER -p$MYSQL_PASS -h$HOST --port=3306 -Bs
+            echo "$SANDBOX_NAME" >> sandboxes_done.txt
 
         fi
     fi
