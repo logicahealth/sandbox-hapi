288b3d14cb4c94b6750ae877a3b2cddf3612bf2b
Shilpy <shilpy@interopion.com>
2020-05-25 15:49:34 -0600

    changed the memory size to 1024
    Former-commit-id: a1c5522fbf1de29b2d40fd2ec9c480129c4a229e [formerly b4e48ba06a02a4b23d188869474f1d0efb09e32e [formerly ca81c5d4c72d955dbaa7d9ef7f97676b028199dc]]
    Former-commit-id: cd3b388b0ec93314839e32609e7d6c3665f6060d
    Former-commit-id: e81383f80bf60ce60e112a35a6ab387bfdab8f7f [formerly 17d3815b44beddae9071ffc7f2770bf31eab30ec]
    Former-commit-id: d512ab11e6c002c8437126e36fd8340b6d268e9c

--- run-local.sh
+++ run-local.sh
@@ -59,6 +59,6 @@ echo "running..."
 java \
   -Dspring.profiles.active=${profile},${tenant},local \
   -Xms256M \
-  -Xmx512M \
+  -Xmx1024M \
   -Dhspc.platform.api.sandbox.name=${sandbox_name} \
   -jar ${jar}
